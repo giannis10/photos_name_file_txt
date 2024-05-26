f4725c2d816d267abfa78d338ba7e87ac47df43e
giannis10 <80069359+giannis10@users.noreply.github.com>
2024-05-27 00:17:36 +0300

    Add files via upload

---
+++ photos_name.py
@@ -0,0 +1,23 @@
+﻿import os
+
+def save_image_filenames(directory, output_file):
+    # Λίστα για να αποθηκεύσουμε τα ονόματα των αρχείων
+    image_filenames = []
+
+    # Διάσχιση του φακέλου
+    for root, dirs, files in os.walk(directory):
+        for file in files:
+            # Έλεγχος αν το αρχείο έχει κατάληξη .jpg ή .png
+            if file.endswith(".JPG") or file.endswith(".PNG"):
+                image_filenames.append(file)
+
+    # Αποθήκευση των ονομάτων σε αρχείο .txt
+    with open(output_file, 'w') as f:
+        for filename in image_filenames:
+            f.write("{}\n".format(filename))
+
+# Παράδειγμα χρήσης
+directory = r'C:\Users\giats\Desktop\my photos website'  # Αντικαταστήστε με τον φάκελο που θέλετε να ελέγξετε
+output_file = 'image_filenames.txt'  # Το όνομα του αρχείου εξόδου
+
+save_image_filenames(directory, output_file)
