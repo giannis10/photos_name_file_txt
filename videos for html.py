5995e7886e38e1f156d24a68a5fbbdeff3e7d0d4
giannis10 <80069359+giannis10@users.noreply.github.com>
2024-05-27 00:20:32 +0300

    Update videos for html.py

--- videos for html.py
+++ videos for html.py
@@ -10,7 +10,7 @@ def save_image_filenames_as_html_list(directory, output_file):
             # Έλεγχος αν το αρχείο έχει κατάληξη 
             if file.endswith(".MP4") or file.endswith(".MOV")or file.endswith(".AVI")or file.endswith(".MKV") or file.endswith(".HEVC") or file.endswith(".VP9") or file.endswith(".AV1") or file.endswith(".mp4") or file.endswith(".mov") or file.endswith(".avi") or file.endswith(".mkv") or file.endswith(".hevc") or file.endswith(".vp9") or file.endswith(".av1"):
                 # Προσθήκη του HTML στοιχείου στη λίστα
-                image_tags.append('<source src="\\192.168.2.70\mi\photos-videos\{}" type="video/mp4">'.format(file))
+                image_tags.append('<source src="{}" type="video/mp4">'.format(file))
 
     # Αποθήκευση των HTML στοιχείων σε αρχείο .txt
     with open(output_file, 'a') as f:
