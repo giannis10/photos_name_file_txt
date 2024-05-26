521356089544eb5505df5df4551ac33776e386bf
giannis10 <80069359+giannis10@users.noreply.github.com>
2024-05-27 00:20:04 +0300

    Update photos for html.py

--- photos for html.py
+++ photos for html.py
@@ -10,7 +10,7 @@ def save_image_filenames_as_html_list(directory, output_file):
             # Έλεγχος αν το αρχείο έχει κατάληξη 
             if file.endswith(".JPG") or file.endswith(".PNG") or file.endswith(".jpg") or file.endswith(".png"):
                 # Προσθήκη του HTML στοιχείου στη λίστα
-                image_tags.append('<li><img src="\\192.168.2.70\mi\photos-videos{}"></li>'.format(file))
+                image_tags.append('<li><img src="{}"></li>'.format(file))
 
     # Αποθήκευση των HTML στοιχείων σε αρχείο .txt
     with open(output_file, 'a') as f:
